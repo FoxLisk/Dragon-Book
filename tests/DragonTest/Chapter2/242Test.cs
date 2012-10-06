58ec7422b200831ec6bc31bc4bc44502df0d5638
Alexander Corwin <ancorwin@gmail.com>
2012-10-06 00:28:16 -0400

    adding more unit tests - feeling good about the S ( S ) S | e parser now!

---
+++ tests/DragonTest/Chapter2/242Test.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Dragons.Chapter2;
+using System.Collections.Generic;
+
+namespace DragonTest
+{
+    [TestClass]
+    public class _242Test
+    {
+        [TestMethod]
+        public void TestSuccseses()
+        {
+            List<String> validStrings = new List<String>()
+            {
+                "","()", "()()", "()(()())", "(())((((()))))","(()(()(()(()))))"
+            };
+            foreach (var code in validStrings)
+            {
+                var parser = new _242(code);
+                parser.Parse();
+            }
+        }
+
+
+        [TestMethod]
+        public void TestFailures()
+        {
+            var invalidStrings = new List<String>() {
+            "(", ")", "())", "(()", ")("
+            };
+            foreach (var code in invalidStrings)
+            {
+                try
+                {
+                    new _242(code).Parse();
+                    Assert.Fail();
+                }
+                catch (InvalidSyntaxException)
+                {
+                }
+            }
+        }
+    }
+}
---
+++ tests/DragonTest/Chapter2/243Test.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Dragons.Chapter2;
+using System.Collections.Generic;
+
+namespace DragonTest
+{
+    [TestClass]
+    public class _243Test
+    {
+        [TestMethod]
+        public void TestSuccseses()
+        {
+            List<String> validStrings = new List<String>()
+            {
+                "01", "0011"
+            };
+            foreach (var code in validStrings)
+            {
+                new _243(code).Parse();
+            }
+        }
+
+
+        [TestMethod]
+        public void TestFailures()
+        {
+            var invalidStrings = new List<String>() {
+            "", "0", "00", "011", "00111"
+            };
+            foreach (var code in invalidStrings)
+            {
+                try
+                {
+                    new _243(code).Parse();
+                    Assert.Fail();
+                }
+                catch (InvalidSyntaxException)
+                {
+                }
+            }
+        }
+    }
+}
