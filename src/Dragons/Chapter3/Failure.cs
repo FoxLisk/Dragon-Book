f0b70a02f902d05ab0c4898b6d46f09b6ed6a058
Alexander Corwin <ancorwin@gmail.com>
2012-11-22 11:31:03 -0500

    adding a failure function computer

---
+++ src/Dragons/Chapter3/Failure.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Dragons.Chapter3
+{
+    public class Failure
+    {
+        string b;
+        public int[] failureFunction(string b)
+        {
+            this.b = b;
+            //b = b1b2b3..bn
+            int n = b.Length + 1;
+            int[] f = new int[n];
+            int t = 0;
+            f[1] = 0;
+            for (int s = 1; s < n - 1; s++)
+            {
+                while (t > 0 && getChar(s + 1) != getChar(t + 1))
+                {
+                    t = f[t];
+                }
+                if (getChar(s + 1) == getChar(t + 1))
+                {
+                    t = t + 1;
+                    f[s + 1] = t;
+                }
+                else
+                {
+                    f[s + 1] = 0;
+                }
+            }
+            return f;
+        }
+        private char getChar(int n)
+        {
+            return b[n - 1];
+        }
+    }
+}
